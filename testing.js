96b26f4d0b901ef72f41591fb6cfa826912c38ec
test-dscrobonia <79946606+test-dscrobonia@users.noreply.github.com>
2021-03-12 12:55:27 -0800

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,2 +1,7 @@
 let x = 1
 eval(x)
+
+// ruleid: javascript.jose.security.jwt-none-alg.jwt-none-alg
+const jose = require("jose");
+const { JWK, JWT } = jose;
+const token = JWT.verify('token-here', JWK.None);
